1bcd71aace0cdee3776fb0e373c747724bca3ca9
Bruno Lemos <brunollemos@live.com>
2023-04-12 11:38:01 -0300

    Aprendendo a tratar erros com o método catch

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var consultaCep = fetch("https://viacep.com.br/ws/01001000/json/")
-.then(resposta => resposta.json())
-.then(r => console.log(r));
+    .then(resposta => resposta.json())
+    .then(r => {
+        if (r.erro) {
+            throw Error("CEP inválido");
+        } else 
+            console.log(r)
+        })
+    .catch(erro => console.log(erro));
 
 console.log(consultaCep);
