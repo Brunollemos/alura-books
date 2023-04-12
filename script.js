1a9d34bb602de7fbd10c166d1d851b311745ec48
Bruno Lemos <brunollemos@live.com>
2023-04-12 14:42:00 -0300

    Exemplo de uso do Promise.all

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-/* var consultaCep = fetch("https://viacep.com.br/ws/01001000/json/")
+async function buscaEndereco(cep) {
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error("CEP inválido");
+        }
+        console.log(consultaCepConvertida);
+        return consultaCepConvertida;
+
+    } catch (erro) {
+        console.log(erro);
+    }    
+}
+
+//exemplo de uso do Promise.all
+let = ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
+
+//exemplo de uso de fetch sem metodo async
+ /*var consultaCep = fetch("https://viacep.com.br/ws/01001000/json/")
     .then(resposta => resposta.json())
     .then(r => {
         if (r.erro) {
@@ -9,23 +31,4 @@
     .catch(erro => console.log(erro))
     .finally(mensagem => console.log("Busca finalizada"));
 
-console.log(consultaCep); */
-
-async function buscaEndereco() {
-
-    try {
-        var consultaCep = await fetch("https://viacep.com.br/ws/01001250/json/");
-        var consultaCepConvertida = await consultaCep.json();
-
-        if (consultaCepConvertida.erro) {
-            throw Error("CEP inválido");
-        }
-        console.log(consultaCepConvertida);
-        
-    } catch (erro) {
-        console.log(erro);
-    }
-    
-}
-
-buscaEndereco();
+console.log(consultaCep);*/
