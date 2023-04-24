2588f31f3f91f04084042e691a383b9a8754181c
Bruno Lemos <brunollemos@live.com>
2023-04-24 18:28:19 -0300

    Padronização de código

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById("erro");
-    mensagemErro.innerHTML = "";
-    try {
-        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCepConvertida = await consultaCep.json();
-        if (consultaCepConvertida.erro) {
-            throw Error("CEP inválido");
-        }
-        var cidade = document.getElementById("cidade");
-        var logradouro = document.getElementById("endereco");
-        var bairro = document.getElementById("bairro");        
-        var estado = document.getElementById("estado");
+	var mensagemErro = document.getElementById("erro");
+	mensagemErro.innerHTML = "";
+	try {
+		var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+		var consultaCepConvertida = await consultaCep.json();
+		if (consultaCepConvertida.erro) {
+			throw Error("CEP inválido");
+		}
+		var cidade = document.getElementById("cidade");
+		var logradouro = document.getElementById("endereco");
+		var bairro = document.getElementById("bairro");        
+		var estado = document.getElementById("estado");
 
-        cidade.value = consultaCepConvertida.localidade;
-        logradouro.value = consultaCepConvertida.logradouro;
-        bairro.value = consultaCepConvertida.bairro;
-        estado.value = consultaCepConvertida.uf;
-        console.log(consultaCepConvertida);
-        return consultaCepConvertida;
+		cidade.value = consultaCepConvertida.localidade;
+		logradouro.value = consultaCepConvertida.logradouro;
+		bairro.value = consultaCepConvertida.bairro;
+		estado.value = consultaCepConvertida.uf;
+		console.log(consultaCepConvertida);
+		return consultaCepConvertida;
 
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente.</p>`;
-        console.log(erro);
-    }    
+	} catch (erro) {
+		mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente.</p>`;
+		console.log(erro);
+	}    
 }
 
 var cep = document.getElementById("cep");
